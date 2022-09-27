05ceadbde257bb0d50d9d3256a2e6dcb3f1d81e9
Shmuel Tzishinsky <a0527188257@gmail.com>
2022-09-28 01:11:09 +0300

    TEST

--- functions/telegram.js
+++ functions/telegram.js
@@ -11,41 +11,21 @@ const { checksTweet } = require("./actions/tweet");
 
 let intrevalChecksTweet;
 let i = 0;
-function test(e) {
-  console.log("TEST");
-  sendMessage(process.env.TELEGRAM_ADMINS, "my intereval is " + i++ + "", "HTML");
-}
 mongoose.connection.once("open", () => {
   console.log("MongoDB Connected!");
-  intrevalChecksTweet = setInterval(test, 1000); // Checks every 1 minutes ---
+
+  setInterval(() => {
+    console.log("TEST " + i++ + "");
+  }, 1500);
+  intrevalChecksTweet = setInterval(checksTweet, 5 * 60 * 1000); // Checks every 1 minutes ---
 });
 
-// 5 * 60 *
-
 // General commands
-// bot.on("text", async (e) => {
-//   if (e.message?.text === "/stop") {
-//     clearInterval(intrevalChecksTweet);
-//     await sendMessage(process.env.TELEGRAM_ADMINS, "I stop the msg", "HTML");
-//     return;
-//   }
-//   if (e.message?.text === "/start") {
-//     intrevalChecksTweet = setInterval(test, 1000);
-//     await sendMessage(process.env.TELEGRAM_ADMINS, "I start the msg", "HTML");
-//     return;
-//   }
-//   userSendMsg(e);
-// });
+// bot.on("text", userSendMsg);
 
 // bot.launch();
 exports.handler = async (event) => {
   try {
-    const { message } = JSON.parse(event.body + "");
-
-    if (message.text === "/stop") {
-      clearInterval(intrevalChecksTweet);
-      return await sendMessage(process.env.TELEGRAM_ADMINS, "I stop the msg", "HTML");
-    }
     await userSendMsg(event.body);
     return { statusCode: 200, body: "" };
   } catch (e) {
