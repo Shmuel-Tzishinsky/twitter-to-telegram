464d9d9f3e96a91ab3e70d717b0fa3005438b201
Shmuel Tzishinsky <a0527188257@gmail.com>
2022-09-28 16:23:53 +0300

    TEST

--- functions/telegram.js
+++ functions/telegram.js
@@ -10,16 +10,10 @@ const { userSendMsg, sendMessage } = require("./actions");
 const { checksTweet } = require("./actions/tweet");
 
 let intrevalChecksTweet;
-async function test(i) {
-  return await sendMessage(process.env.TELEGRAM_ADMINS, `server ${i}`, "HTML");
-}
 mongoose.connection.once("open", () => {
   console.log("MongoDB Connected!");
-  let i = 0;
-  intrevalChecksTweet = setInterval(() => test(i++), 1000); // Checks every 1 minutes ---
+  intrevalChecksTweet = setInterval(checksTweet, 5 * 60 * 1000); // Checks every 1 minutes ---
 });
-
-// 5 * 60 *
 
 // General commands
 // bot.on("text", userSendMsg);
